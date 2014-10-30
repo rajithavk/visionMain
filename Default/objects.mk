2be8200247b7abe98ee3db5ec31e9d22bcb52298
rajithavk <slamdamjc@gmail.com>
2014-10-30 20:44:44 +0530

    new

---
+++ Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_nonfree -lopencv_core -lopencv_features2d -lopencv_highgui
+
---
+++ Default/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../BOF.cpp \
+../functions.cpp 
+
+OBJS += \
+./BOF.o \
+./functions.o 
+
+CPP_DEPS += \
+./BOF.d \
+./functions.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/local/include/opencv2 -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
